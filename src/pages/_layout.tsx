ebbce224dcf65c056a7e8abb0ef8658e8139055b
pySoo <soopy368@gmail.com>
2024-03-03 18:33:33 +0900

    [feat] : Fallback 컴포넌트를 Skeleton으로 변경

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -4,8 +4,8 @@ import { Outlet } from 'react-router-dom';
 
 import ErrorFallback from '@/components/common/ErrorFallback';
 import Header from '@/components/common/Header';
-import Loading from '@/components/common/Loading';
 import Navbar from '@/components/common/Navbar';
+import SkeletonBoard from '@/components/common/SkeletonBoard';
 import ToastContainer from '@/components/common/ToastContainer';
 import ModalContainer from '@/components/Modal/ModalContainer';
 import { useToggle } from '@/hooks/useToggle';
@@ -18,7 +18,7 @@ export default function Layout() {
       <Navbar isNavOpen={isToggled} onToggle={toggle} />
 
       <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <Suspense fallback={<Loading />}>
+        <Suspense fallback={<SkeletonBoard />}>
           <Outlet />
         </Suspense>
       </ErrorBoundary>
